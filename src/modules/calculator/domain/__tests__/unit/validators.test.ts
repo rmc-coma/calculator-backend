db4097ab7ed8ffa23139fba0cf5bbf5dce8addb5
Raphaël MC COMAS <raphael.mccomas@scorechain.com>
2023-01-08 20:08:13 +0100

    Added one more test for calculator validator

--- src/modules/calculator/domain/__tests__/unit/validators.test.ts
+++ src/modules/calculator/domain/__tests__/unit/validators.test.ts
@@ -13,9 +13,19 @@ describe("Calculator validator", () => {
         expect(res).toBe(false);
     });
 
-    it("should accept a term suite not alternating numbers and operators", () => {
+    it("should deny a term suite not alternating numbers and operators", () => {
         const terms = [2, "*", 2.5, 3, "-", 2, "/", 2, "+", 3, "*", 4, "*", 2, "/", 2];
         const res = CalculatorDomain.Validators.validateCalculateInput(terms);
+        expect(res).toBe(false);
+    });
+
+    it("should deny a anything other than a term suite", () => {
+        const terms = { key: "Hello world !" };
+        const res = CalculatorDomain.Validators.validateCalculateInput(
+            terms as unknown as Parameters<
+                (typeof CalculatorDomain)["Validators"]["validateCalculateInput"]
+            >[0]
+        );
         expect(res).toBe(false);
     });
 });
